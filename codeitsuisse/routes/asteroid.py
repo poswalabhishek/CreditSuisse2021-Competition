01453ca59f600ceec5e666ea6b6a9089e565d524
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-25 15:13:51 +0800

    updates

--- codeitsuisse/routes/asteroid.py
+++ codeitsuisse/routes/asteroid.py
@@ -78,7 +78,7 @@ def evaluateAsteroidScore():
         '''
         case_output = {}
         case_output['input'] = inputString
-        logging.info("Output :{}".format(case_input))
+        logging.info("Output :{}".format(case_output))
 
         input_string = inputString + 'x'
         asteroid_type_and_value = []
@@ -90,7 +90,7 @@ def evaluateAsteroidScore():
         while i != len(input_string) - 1:
             asteroid_value = 0
             
-            while curr_asteroid == next_asteroid and input_string[i+1] != 'x':
+            while curr_asteroid == next_asteroid and (input_string[i] != 'x' or input_string[i+1] != 'x'):
                 asteroid_value += 1
                 i += 1
                 curr_asteroid = input_string[i]
