a9531cd613d3f0daebf2792e32db6c54b698847f
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-26 02:13:02 +0800

    commit

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -7,7 +7,7 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/decoder', methods=['POST'])
+
 
 def right_and_wrong_symbols_position_result(result):
     right_symbol_right_position = result % 10
@@ -15,11 +15,15 @@ def right_and_wrong_symbols_position_result(result):
 
     return right_symbol_right_position, right_symbol_wrong_position
 
+@app.route('/decoder', methods=['POST'])
+
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    result = data["history"]["output_received"]
+    result = {}
+    
+    result['answer'] = data["history"]["output_received"]
 
     result = random.sample(result, len(result))
 
