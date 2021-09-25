8040a85e3dae762f0f40e5f08e69b55d7507bed4
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-25 22:16:09 +0800

    fixed_race --initial commit

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.asteroid
 import codeitsuisse.routes.options_optimization
+import codeitsuisse.routes.fixed_race
 # import the file that we have just created
---
+++ codeitsuisse/routes/fixed_race.py
@@ -0,0 +1,23 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+# def winner_guesses():
+
+
+@app.route('/fixedrace', methods=['POST'])
+
+def evaluateFixedRace():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input")
+
+    return data
+
+
+
