e4e40af83b84d6da2b66ead89b1defa6e30fa15d
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-26 02:33:57 +0800

    commit

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -1,36 +1,13 @@
 import logging
 import json
-import random
+
 from flask import request, jsonify
 
 from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-
-
-# def right_and_wrong_symbols_position_result(result):
-#     right_symbol_right_position = result % 10
-#     right_symbol_wrong_position = (result // 10) % 10
-
-#     return right_symbol_right_position, right_symbol_wrong_position
-
 @app.route('/decoder', methods=['POST'])
-
-# def evaluateDecoder():
-#     data = request.get_json()
-#     logging.info("data sent for evaluation {}".format(data))
-
-#     result = {}
-    
-#     result['answer'] = data['history'][0]['output_received']
-
-#     result = random.sample(result, len(result))
-
-#     logging.info("My result :{}".format(result))
-#     return json.dumps(result)
-
-
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
@@ -38,5 +15,6 @@ def evaluateDecoder():
     result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
+
 
 
