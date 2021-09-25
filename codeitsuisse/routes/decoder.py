753594a3d2537399f6fa61567964ebea5a4db3ce
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-26 02:41:28 +0800

    push

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -8,12 +8,14 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/decoder', methods=['POST'])
-def evaluateDecoder():
+def evaluateDecoder ():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
+
+    output = data["history"][0]["output_received"]
+    result = {}
+    result['answer'] = random.sample(output, len(output))
+
     return json.dumps(result)
 
 
