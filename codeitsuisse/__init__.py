5d269509ba47034501e5cef1d03b614240c696e9
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-26 08:08:37 +0800

    stig_one --initial commit

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -6,4 +6,5 @@ import codeitsuisse.routes.options_optimization
 import codeitsuisse.routes.fixed_race
 import codeitsuisse.routes.decoder
 import codeitsuisse.routes.cipher
+import codeitsuisse.routes.stig_one
 # import the file that we have just created
