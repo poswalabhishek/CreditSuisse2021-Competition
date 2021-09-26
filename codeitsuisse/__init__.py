b78e2efd65ff889b6fe210e1f6ad448906a56f7d
poswalabhishek <poswalabhishek1234@icloud.com>
2021-09-26 08:15:15 +0800

    decode --fixing errors

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,7 +4,7 @@ import codeitsuisse.routes.square
 import codeitsuisse.routes.asteroid
 import codeitsuisse.routes.options_optimization
 import codeitsuisse.routes.fixed_race
-import codeitsuisse.routes.decoder
+import codeitsuisse.routes.decode
 import codeitsuisse.routes.cipher
 import codeitsuisse.routes.stig_one
 # import the file that we have just created
